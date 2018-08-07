31e3dda1460f91d9905cfd24c6da8737dd3bdc2e
Better Better <jwsxzx@naver.com>
2018-08-08 03:27:11 +0900

    Update ShellBag_Parser.py

--- ShellBag_Parser.py
+++ ShellBag_Parser.py
@@ -1,22 +1,47 @@
 from winreg import *
+from datetime import datetime, timedelta
 import struct
+
+cut = 0
+
+def Win_ts(timestamp):
+    WIN32_EPOCH = datetime(1601, 1, 1)
+    return WIN32_EPOCH + timedelta(microseconds=timestamp//10, hours=9)
+
+def loop_Sub_Parser(Var_Key, Path, Sub_Key_Cut, cut):
+	for i in range(Sub_Key_Cut):
+		try:
+			name, data, x = EnumValue(Var_Key, i)
+			if not name in ("MRUListEx", "NodeSlot"):
+				Key_Path = Path + r'\\' + name
+				loop_Sub_Key = OpenKey(Var_Hive, Path)
+				ts = QueryInfoKey(loop_Sub_Key)[2]
+				print(' '*cut +'{} {}'.format(Key_Path,Win_ts(ts)))
+				if(QueryInfoKey(Var_Key)[0] != 3):
+					cut+=1
+					loop_Sub_Parser(Var_Key, Key_Path, QueryInfoKey(Var_Key)[0], cut)	
+		except WindowsError:
+			pass
 
 BagMRU_Path = r'Software\\Classes\\Local Settings\\Software\\Microsoft\\Windows\\Shell\\BagMRU'
 Var_Hive = ConnectRegistry(None, HKEY_CURRENT_USER)
 Var_Key = OpenKey(Var_Hive, BagMRU_Path)
-
 BagMRU_Cut = QueryInfoKey(Var_Key)[0]
 
 for i in range(BagMRU_Cut):
 	try:
+		cut=0
 		name, data, x = EnumValue(Var_Key, i)
-		if not name in ("MRULISTEX", "NodeSlot", "NodeSlots"):
-			Sub_BagMRU_Path = BagMRU_Path + r'\\' + str(i)
-			
-			ts = QueryInfoKey(a)[2]
-			print(ts)
-	
-	except:
+		if not name in ("MRUListEx", "NodeSlot", "NodeSlots"):
+			Sub_BagMRU_Path = BagMRU_Path + r'\\' + name
+			Sub_Var_Key = OpenKey(Var_Hive, Sub_BagMRU_Path)
+			ts = QueryInfoKey(Sub_Var_Key)[2]
+			print(Sub_BagMRU_Path, name, Win_ts(ts))
+			if(QueryInfoKey(Sub_Var_Key)[0] != 3):
+				cut+=1
+				loop_Sub_Parser(Sub_Var_Key, Sub_BagMRU_Path, QueryInfoKey(Sub_Var_Key)[0], cut)
+
+	except WindowsError:
 		pass
 
 # try:
