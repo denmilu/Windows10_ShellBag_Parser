c9fe098e7c61a335a0b6f5a20e27ee89eed6419a
Better Better <jwsxzx@naver.com>
2018-08-09 02:42:05 +0900

    Path, Sub_Path, Modfied_Time Print

--- ShellBag_Parser.py
+++ ShellBag_Parser.py
@@ -13,20 +13,24 @@ def loop_Sub_Parser(Var_Key, Path, Sub_Key_Cut, cut):
 		try:
 			name, data, x = EnumValue(Var_Key, i)
 			if not name in ("MRUListEx", "NodeSlot"):
-				Key_Path = Path + r'\\' + name
-				loop_Sub_Key = OpenKey(Var_Hive, Path)
+				Key_Path = Path + r'\\' + name 
+				loop_Sub_Key = OpenKey(Var_Hive, Key_Path)
 				ts = QueryInfoKey(loop_Sub_Key)[2]
-				print(' '*cut +'{} {}'.format(Key_Path,Win_ts(ts)))
-				if(QueryInfoKey(Var_Key)[0] != 3):
+				print(' '*cut +'{} {} {} {}'.format(Key_Path, name, Win_ts(ts), cut))
+				if(QueryInfoKey(loop_Sub_Key)[1] != 2):
 					cut+=1
-					loop_Sub_Parser(Var_Key, Key_Path, QueryInfoKey(Var_Key)[0], cut)	
+					loop_Sub_Parser(loop_Sub_Key, Key_Path, QueryInfoKey(Var_Key)[1], cut)
+					cut-=1
+				else:
+					break
+
 		except WindowsError:
 			pass
 
 BagMRU_Path = r'Software\\Classes\\Local Settings\\Software\\Microsoft\\Windows\\Shell\\BagMRU'
 Var_Hive = ConnectRegistry(None, HKEY_CURRENT_USER)
-Var_Key = OpenKey(Var_Hive, BagMRU_Path)
-BagMRU_Cut = QueryInfoKey(Var_Key)[0]
+Var_Key = OpenKey(Var_Hive, BagMRU_Path) 
+BagMRU_Cut = QueryInfoKey(Var_Key)[0] # BagMRU의 요소 개수
 
 for i in range(BagMRU_Cut):
 	try:
@@ -36,13 +40,18 @@ for i in range(BagMRU_Cut):
 			Sub_BagMRU_Path = BagMRU_Path + r'\\' + name
 			Sub_Var_Key = OpenKey(Var_Hive, Sub_BagMRU_Path)
 			ts = QueryInfoKey(Sub_Var_Key)[2]
-			print(Sub_BagMRU_Path, name, Win_ts(ts))
-			if(QueryInfoKey(Sub_Var_Key)[0] != 3):
+			print(Sub_BagMRU_Path, name, Win_ts(ts), cut)
+
+			if(QueryInfoKey(Sub_Var_Key)[1] != 2):
 				cut+=1
-				loop_Sub_Parser(Sub_Var_Key, Sub_BagMRU_Path, QueryInfoKey(Sub_Var_Key)[0], cut)
+				loop_Sub_Parser(Sub_Var_Key, Sub_BagMRU_Path, QueryInfoKey(Sub_Var_Key)[1], cut)
+				cut-=1
 
 	except WindowsError:
 		pass
+
+# c = QueryInfoKey(Var_Key)[1]
+# print(c)
 
 # try:
 # 	cut = 0
