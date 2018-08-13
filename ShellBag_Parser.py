0083ccf10f7a3a9d8af68997e1201169313b661c
Better Better <jwsxzx@naver.com>
2018-08-14 02:55:09 +0900

    Update ShellBag_Parser.py

--- ShellBag_Parser.py
+++ ShellBag_Parser.py
@@ -14,8 +14,8 @@ def loop_Sub_Parser(Sub_Var_Key, Sub_Key_Path, Sub_Key_Cut, cut):
 				loop_Key_Path = Sub_Key_Path + r'\\' + name # 서브키에 하위 키 경로를 추가 해준다.
 				loop_Sub_Key = OpenKey(Var_Hive, loop_Key_Path) # 서브키 핸들러 생성
 				ts = QueryInfoKey(loop_Sub_Key)[2] # 작성된 시간
-				print(' '*cut +'{} {} {} {}'.format(loop_Key_Path, name, Win_ts(ts), cut))
-				Itempos_Size(data)
+				print(' '*cut +'{} {} {} {}'.format(loop_Key_Path, name, ts, cut))
+				print(' '*cut + 'Itempos_Size : {} File_Size : {} Created_Date : {} Modified_Date : {} File_Name : {}'.format(Itempos_Size(data), File_Size(data), Created_Date(data), Modified_Date(data), File_Name(data)))
 
 				if(QueryInfoKey(loop_Sub_Key)[1] != 2): # 하위키의 값이 더 있을 경우
 					cut+=1
@@ -37,8 +37,8 @@ def BagMRU_Parser():
 				Sub_BagMRU_Path = BagMRU_Path + r'\\' + name # 하위키의 값 경로 만들기
 				Sub_Var_Key = OpenKey(Var_Hive, Sub_BagMRU_Path) # 하위키 핸들러
 				ts = QueryInfoKey(Sub_Var_Key)[2]
-				print(Sub_BagMRU_Path, name, Win_ts(ts), cut)
-				Itempos_Size(data)
+				print(Sub_BagMRU_Path, name, ts ,Itempos_Size(data), cut)
+				print('Itempos_Size : {} File_Size : {} Created_Date : {} Modified_Date : {} File_Name : {}'.format(Itempos_Size(data), File_Size(data), Created_Date(data), Modified_Date(data),File_Name(data)))
 
 				if(QueryInfoKey(Sub_Var_Key)[1] != 2): # 하위키의 값이 2개가 아닐 경우 (2개보다 많을 경우)
 					cut+=1
@@ -48,9 +48,29 @@ def BagMRU_Parser():
 		except WindowsError:
 			pass
 
-		
 def Itempos_Size(data):
-	print(data[:2])
+	try:
+		return abs(struct.unpack_from('b', data[0:2].replace(b"\x00",b""))[0])
+	except:
+		return abs(struct.unpack_from('h', data[0:2].replace(b"\x00",b""))[0])
+
+def File_Size(data):
+	return data[4:8].replace(b"\x00", b"")
+
+def Created_Date(data):
+	try:
+		return Win_ts(struct.unpack_from('<L', data[32:36])[0])
+	except:
+		return data[32:36]
+
+def Modified_Date(data):
+	try:
+		return Win_ts(struct.unpack_from('<L', data[36:40])[0])
+	except:
+		return data[36:40]
+
+def File_Name(data):
+	return data[0x0E:0x22].replace(b'\x00', b'')
 
 BagMRU_Path = r'Software\\Classes\\Local Settings\\Software\\Microsoft\\Windows\\Shell\\BagMRU'
 Var_Hive = ConnectRegistry(None, HKEY_CURRENT_USER)
@@ -58,15 +78,3 @@ Var_Key = OpenKey(Var_Hive, BagMRU_Path) # BagMRU 핸들러
 BagMRU_Cut = QueryInfoKey(Var_Key)[1] # BagMRU의 요소 개수
 
 BagMRU_Parser()
-
-# c = QueryInfoKey(Var_Key)[1]
-# print(c)
-
-# try:
-# 	cut = 0
-# 	while True:
-# 		Data = QueryValueEx(Wininfo_Key, str(cut))[0]
-# 		print(struct.unpack_from("<B",Data[14:21])[0])
-# 		cut += 1
-# except WindowsError:
-# 	pass
