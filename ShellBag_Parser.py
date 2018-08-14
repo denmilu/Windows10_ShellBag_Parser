405494bfd8f5060e073bdf47bb213564bf8a8daf
Better Better <jwsxzx@naver.com>
2018-08-14 12:17:36 +0900

    Update ShellBag_Parser.py

--- ShellBag_Parser.py
+++ ShellBag_Parser.py
@@ -12,19 +12,19 @@ def loop_Sub_Parser(Sub_Var_Key, Sub_Key_Path, Sub_Key_Cut, cut):
 		try:
 			name, data, x = EnumValue(Sub_Var_Key, i)
 			if not name in ("MRUListEx", "NodeSlot"):
-				loop_Key_Path = Sub_Key_Path + r'\\' + name # 서브키에 하위 키 경로를 추가 해준다.
-				loop_Sub_Key = OpenKey(Var_Hive, loop_Key_Path) # 서브키 핸들러 생성
+				loop_Key_Path = Sub_Key_Path + r'\\' + name 
+				loop_Sub_Key = OpenKey(Var_Hive, loop_Key_Path) 
 				ts = QueryInfoKey(loop_Sub_Key)[2] # 작성된 시간
 				print(' '*cut +'{} {} {} {}'.format(loop_Key_Path, name, Win_ts(ts).strftime('%Y:%m:%d_%H:%M:%S.%f'), cut))
 				print(' '*cut + 'Itempos_Size : {} File_Size : {} Created_Date : {} Modified_Date : {} File_Name : {}'.format(Itempos_Size(data), File_Size(data), Created_Date(data), Modified_Date(data), File_Name(data)))
 				
 				wr.writerow([loop_Key_Path, Win_ts(ts).strftime('%Y:%m:%d_%H:%M:%S.%f'), Itempos_Size(data), File_Size(data), Created_Date(data), Modified_Date(data) ,File_Name(data)])
 
-				if(QueryInfoKey(loop_Sub_Key)[1] != 2): # 하위키의 값이 더 있을 경우
+				if(QueryInfoKey(loop_Sub_Key)[1] != 2):
 					cut+=1
-					loop_Sub_Parser(loop_Sub_Key, loop_Key_Path, QueryInfoKey(loop_Sub_Key)[1], cut) # 한번더 loop_Sub_Parser 실행
+					loop_Sub_Parser(loop_Sub_Key, loop_Key_Path, QueryInfoKey(loop_Sub_Key)[1], cut)
 					cut-=1
-				else: # 하위키의 값이 없을 경우 for문 종료
+				else: 
 					break
 
 		except WindowsError:
@@ -33,21 +33,21 @@ def loop_Sub_Parser(Sub_Var_Key, Sub_Key_Path, Sub_Key_Cut, cut):
 def BagMRU_Parser():
 	for i in range(BagMRU_Cut): # BagMRU 값 개수
 		try:
-			cut = 0 # 하위키의 값을 보기 편하게 하기 위해 카운트
+			cut = 0
 			name, data, x = EnumValue(Var_Key, i)
 
 			if not name in ("MRUListEx", "NodeSlot", "NodeSlots"):
-				Sub_BagMRU_Path = BagMRU_Path + r'\\' + name # 하위키의 값 경로 만들기
-				Sub_Var_Key = OpenKey(Var_Hive, Sub_BagMRU_Path) # 하위키 핸들러
+				Sub_BagMRU_Path = BagMRU_Path + r'\\' + name 
+				Sub_Var_Key = OpenKey(Var_Hive, Sub_BagMRU_Path)
 				ts = QueryInfoKey(Sub_Var_Key)[2]
 				print(Sub_BagMRU_Path, name, Win_ts(ts).strftime('%Y:%m:%d_%H:%M:%S.%f') , cut)
 				print('Itempos_Size : {} File_Size : {} Created_Date : {} Modified_Date : {} File_Name : {}'.format(Itempos_Size(data), File_Size(data), Created_Date(data), Modified_Date(data),File_Name(data)))
 				
 				wr.writerow([Sub_BagMRU_Path, Win_ts(ts).strftime('%Y:%m:%d_%H:%M:%S.%f'), Itempos_Size(data), File_Size(data), Created_Date(data), Modified_Date(data) ,File_Name(data)])
 				
-				if(QueryInfoKey(Sub_Var_Key)[1] != 2): # 하위키의 값이 2개가 아닐 경우 (2개보다 많을 경우)
+				if(QueryInfoKey(Sub_Var_Key)[1] != 2): 
 					cut+=1
-					loop_Sub_Parser(Sub_Var_Key, Sub_BagMRU_Path, QueryInfoKey(Sub_Var_Key)[1], cut) # loop_Sub_Parser 호출
+					loop_Sub_Parser(Sub_Var_Key, Sub_BagMRU_Path, QueryInfoKey(Sub_Var_Key)[1], cut)
 					cut-=1
 
 		except WindowsError:
@@ -83,8 +83,8 @@ wr.writerow(["Reg_Path", "Reg_Modified_Time", "Itempos_Size", "File_Size", "Crea
 
 BagMRU_Path = r'Software\\Classes\\Local Settings\\Software\\Microsoft\\Windows\\Shell\\BagMRU'
 Var_Hive = ConnectRegistry(None, HKEY_CURRENT_USER)
-Var_Key = OpenKey(Var_Hive, BagMRU_Path) # BagMRU 핸들러
-BagMRU_Cut = QueryInfoKey(Var_Key)[1] # BagMRU의 요소 개수
+Var_Key = OpenKey(Var_Hive, BagMRU_Path) 
+BagMRU_Cut = QueryInfoKey(Var_Key)[1] 
 
 BagMRU_Parser()
 
